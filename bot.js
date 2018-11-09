3d1a0d7d36d934036d22a1ae76aa5fc8eb8f959d
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-09 17:12:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "S5";
 
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
@@ -14,4 +15,21 @@ var channel = "508326742314844181";//ايدي الروم
     },305);
 })
 
-client.login('NTA4MzQ4NTE1NTY3Nzk2MjM1.Dr-XOg.nas4GMqce3npN0De8H4fHxX0ZsA');
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+client.login('NTA4MzQ4NTE1NTY3Nzk2MjM1.DsdDcQ.WUyMFOSqIFw8rn6lpL34vxovUoE');
